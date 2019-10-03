0d0299669c6ffa4d6875ddf65f0708f6afea65ae
Ahammad Sabir <sabir.cse5.bu@gmail.com>
2019-10-03 13:48:11 +0600

    Update Sort_Merge Sort.cpp

--- Sort_Merge Sort.cpp
+++ Sort_Merge Sort.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void Merge();
-void Mergesort();
+#define spc " "
+#define nl "\n"
+#define FOR(n) for(int i = 0; i < n; i++)
+
+void Merge(int arr[], int l, int r);
+void Mergesort(int arr[], int l, int r);
+void Sort(int arr[], int n)
+{
+    //Mergesort();
+}
+
 int main()
+{
+    int arr[] = {6,5,2,7,3,2,1,0};
+    int n = 8;
+
+}
+
+void Merge(int arr[], int l, int r)
+{
+
+}
+void Mergesort(int arr[], int l, int r)
 {
 
 }
