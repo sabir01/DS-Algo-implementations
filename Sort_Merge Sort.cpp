1476c66ae0f3f73f893f33d35b30819cc6f11c46
Ahammad Sabir <sabir.cse5.bu@gmail.com>
2019-10-06 01:21:29 +0600

    Update Sort_Merge Sort.cpp

--- Sort_Merge Sort.cpp
+++ Sort_Merge Sort.cpp
@@ -2,16 +2,9 @@
 
 using namespace std;
 
-#define spc " "
-#define nl "\n"
-#define FOR(n) for(int i = 0; i < n; i++)
-
 void Merge(int arr[], int l, int m, int r);
 void Mergesort(int arr[], int l, int r);
-void Sort(int arr[], int n)
-{
-    Mergesort(arr, 0, n - 1);
-}
+void Sort(int arr[], int n);
 
 int main()
 {
@@ -25,76 +18,50 @@ int main()
         cout << arr[i] << " ";
     }
 }
-
 void Merge(int arr[], int l, int m, int r)
 {
     int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-//    creating the
-//    left array
-//    and the
-//    right array
+    int n1 = m - l + 1, n2 = r - m;
     int L[n1], R[n2];
-
 
     for(i = 0; i < n1; i++)
     {
-        L[i] = arr[l+i];
+        L[i] = arr[l + i];
     }
     for(i = 0; i < n2; i++)
     {
-        R[i] = arr[m+i+1];
+        R[i] = arr[m + i + 1];
     }
+
     i = j = 0;
     k = l;
-//    this while loop actually does the sorting, or more precisely
-//    puts the smaller elements on the left side
-//    and
-//    bigger elements on the right side
+    ///putting the small to the left and big to the right
     while(i < n1 && j < n2)
     {
         if(L[i] <= R[j])
-        {
-            arr[k] = L[i];
-            i++;
-        }
+            arr[k++] = L[i++];
         else
-        {
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
+            arr[k++] = R[j++];
     }
-//    putting the rest of the elements that didn't
-//    get assigned to some space
-//    gets their place
-//    in
-//    the array
+    ///dealing with the leftover
     while(i < n1)
-    {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
+        arr[k++] = L[i++];
     while(j < n2)
-    {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
-
-
+        arr[k++] = R[j++];
 }
 void Mergesort(int arr[], int l, int r)
 {
     if(l < r)
     {
-        int m = l + (r - l) / 2;
+        int mid = ( l + r ) / 2;
 
-        Mergesort(arr, l, m);
-        Mergesort(arr, m+1, r);
+        Mergesort(arr, l, mid);
+        Mergesort(arr, mid + 1, r);
 
-        Merge(arr, l, m, r);
+        Merge(arr, l, mid, r);
     }
+}
+void Sort(int arr[], int n)
+{
+    Mergesort(arr,0,n-1);
 }
