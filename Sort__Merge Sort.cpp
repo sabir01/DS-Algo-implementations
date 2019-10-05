5fd460c2d5834d4ca2d1d5f09e02a749e1429691
Ahammad Sabir <sabir.cse5.bu@gmail.com>
2019-10-05 23:25:23 +0600

    Update Sort__Merge Sort.cpp

--- Sort__Merge Sort.cpp
+++ Sort__Merge Sort.cpp
@@ -6,11 +6,11 @@ using namespace std;
 #define nl "\n"
 #define FOR(n) for(int i = 0; i < n; i++)
 
-void Merge(int arr[], int l, int r);
+void Merge(int arr[], int l, int m, int r);
 void Mergesort(int arr[], int l, int r);
 void Sort(int arr[], int n)
 {
-    //Mergesort();
+    Mergesort(arr, 0, n - 1);
 }
 
 int main()
@@ -18,13 +18,72 @@ int main()
     int arr[] = {6,5,2,7,3,2,1,0};
     int n = 8;
 
+    Sort(arr,n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
 
-void Merge(int arr[], int l, int r)
+void Merge(int arr[], int l, int m, int r)
 {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for(i = 0; i < n1; i++)
+    {
+        L[i] = arr[l+i];
+    }
+    for(i = 0; i < n2; i++)
+    {
+        R[i] = arr[m+i+1];
+    }
+    i = j = 0;
+    k = l;
+
+    while(i < n1 && j < n2)
+    {
+        if(L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+
 
 }
 void Mergesort(int arr[], int l, int r)
 {
+    if(l < r)
+    {
+        int m = l + (r - l) / 2;
 
+        Mergesort(arr, l, m);
+        Mergesort(arr, m+1, r);
+
+        Merge(arr, l, m, r);
+    }
 }
