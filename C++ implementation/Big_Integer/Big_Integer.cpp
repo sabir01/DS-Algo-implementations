67426bad176677b5b056965d0d6bbe035cae77b7
Ahammad Sabir <sabir.cse5.bu@gmail.com>
2020-07-13 23:55:38 +0600

    Added few operator overloading

--- C++ implementation/Big_Integer/Big_Integer.cpp
+++ C++ implementation/Big_Integer/Big_Integer.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 #define BigInt vector<int>
 #define pb push_back
+#define FOR(n)    for(int i = 0; i < n; i++)
+#define REV(n)    for(int i = n; i >= 0; i--)
+int base = 10;
 
 void Set(BigInt &a)
 {
@@ -49,7 +52,84 @@ BigInt Integer(long long n)
 // {
 //     return Integer(to_string(n));
 // }
+void operator >> (istream &in, BigInt &a)
+{
+    string s;
+    getline(cin, s);
+    a = Integer(s);
+}
+void operator << (ostream &out, BigInt a)
+{
+    Print(a);
+}
+bool operator < (BigInt a, BigInt b)
+{
+    if(a.size() != b.size())    return (a.size() < b.size());
+    REV(a.size() - 1)
+    {
+        if(a[i] != b[i])    return (a[i] < b[i]);
+    }
+    return false;
+}
+bool operator > (BigInt a, BigInt b)
+{
+    return (b < a);
+}
+bool operator == (BigInt a, BigInt b)
+{
+    return (!(a<b) && !(b<a));
+}
+bool operator <= (BigInt a, BigInt b)
+{
+    return (a < b || a == b);
+}
+bool operator >= (BigInt a, BigInt b)
+{
+    return (a > b || a == b);
+}
 
+BigInt operator + (BigInt a, BigInt b)
+{
+    Set(a);
+    Set(b);
+    BigInt ans;
+    int carry;
+    FOR(max(a.size(), b.size()) - 1)
+    {
+        if(i < a.size())    carry += a[i];
+        if(i < b.size())    carry += b[i];
+        ans.pb(carry % base);
+        carry /= base;
+    }
+    if(carry)   ans.pb(carry);
+    return ans;
+}
+BigInt operator + (BigInt a, int b)
+{
+    return a + Integer(b);
+}
+BigInt operator ++ (BigInt &a)
+{
+    a = a + Integer(1);
+}
+BigInt operator += (BigInt &a, BigInt b)
+{
+    a = a + b;
+}
+BigInt operator += (BigInt &a, int b)
+{
+    a = a + Integer(b);
+}
+//  -- - / * % 
+
+BigInt operator - (BigInt a, BigInt b)
+{
+    Set(a);
+    Set(b);
+    BigInt ans;
+    int carry;
+    
+}
 int main()
 {
     BigInt a;
